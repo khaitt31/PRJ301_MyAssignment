e10716baae73b006b1e73a67093db06370fae85f
khaitt31 <ttk312004@gmail.com>
2024-10-14 01:25:55 +0700

    control

--- MyProject/src/java/model/Department.java
+++ MyProject/src/java/model/Department.java
@@ -4,10 +4,49 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author ADMIN
+ * @author sonnt-local
  */
 public class Department {
+    private int id;
+    private String name;
+    private ArrayList<Employee> emps = new ArrayList<>();
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Employee> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(ArrayList<Employee> emps) {
+        this.emps = emps;
+    }
+    
+            
 }
