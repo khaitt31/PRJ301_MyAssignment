5adc672c04d21edfb5355faaa78504093c7ac67b
khaitt31 <ttk312004@gmail.com>
2024-11-01 01:13:50 +0700

    cần chỉnh lại productionplan

--- MyProject/src/java/controller/ProductionPlan/ProductionPlanCreateController.java
+++ MyProject/src/java/controller/ProductionPlan/ProductionPlanCreateController.java
@@ -27,6 +27,15 @@ import model.productionplan.Department;
  */
 public class ProductionPlanCreateController extends HttpServlet {
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -37,6 +46,14 @@ public class ProductionPlanCreateController extends HttpServlet {
         request.getRequestDispatcher("../view/productionplan/create.jsp").forward(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -48,6 +65,7 @@ public class ProductionPlanCreateController extends HttpServlet {
 
         Department d = new Department();
         d.setId(Integer.parseInt(request.getParameter("did")));
+
         plan.setDept(d);
         plan.setCampains(new ArrayList<>());
 
@@ -62,7 +80,6 @@ public class ProductionPlanCreateController extends HttpServlet {
             c.setQuantity(raw_quantity != null && raw_quantity.length() > 0 ? Integer.parseInt(raw_quantity) : 0);
             c.setEstimatedeffort(raw_effort != null && raw_effort.length() > 0 ? Float.parseFloat(raw_effort) : 0);
             c.setPlan(plan);
-
             if (c.getQuantity() != 0 && c.getEstimatedeffort() != 0) {
                 plan.getCampains().add(c);
             }
@@ -75,7 +92,8 @@ public class ProductionPlanCreateController extends HttpServlet {
         } else {
             request.getSession().setAttribute("message", "Your plan did not have any campaigns.");
         }
-
         response.sendRedirect("create");
+        
     }
+
 }
